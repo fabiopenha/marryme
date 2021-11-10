d3bb2416892627581c16b76439de0fed9fbb420d
Fábio Penha <fabiopenha95@outlook.com>
2021-11-10 13:27:22 -0300

    first commit

---
+++ apps/accounts/models.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Usuario(models.Model):
+    empresa = models.CharField(max_length=100, verbose_name='Empresa')
+    nome = models.CharField(max_length=100, verbose_name='Nome')
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='Usuário')
+    telefone = models.CharField(max_length=100, verbose_name='Telefone')
+    CATEGORIAS_CHOICES = (
+        ('Salão de Festa', 'Salão de Festa'),
+        ('Espaço para Cerimônia', 'Espaço para Cerimônia'),
+        ('Equipe de Beleza', 'Equipe de Beleza'),
+        ('Equipe de Fotografia e Filmasgens', 'Equipe de Fotografia e Filmagens'),
+        ('Buffet', 'Buffet'),
+        ('Lua de mel', 'Lua de mel'),
+    )
+    categoria = models.CharField(max_length=100, choices= CATEGORIAS_CHOICES)
+
+    def __str__(self):
+        return self.usuario.username
+
---
+++ apps/accounts/signals.py
@@ -0,0 +1,13 @@
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from apps.accounts.models import Usuario
+
+
+@receiver(post_save,sender=User)
+def create_usuario(sender, instance, created, **kwargs):
+    if created:
+        Usuario.objects.create(usuario = instance)
+    else:
+        if not hasattr(instance,"usuario"):
+            Usuario.objects.create(usuario=instance)
